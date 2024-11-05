dbda7633fde01f47ec1122806cf99f2cbf35b3b9
Julia Olle roset <julolle-@car3s3.42barcelona.com>
2024-11-05 20:39:06 +0100

    update structure

--- srcs/user-mgmt/ping_pong/urls.py
+++
@@ -1,8 +0,0 @@
-# ping_pong/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Ensure 'index' is the correct view function name
-]
-
--- srcs/user-mgmt/ping_pong/views.py
+++
@@ -1,5 +0,0 @@
-# ping_pong/views.py
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'ping_pong/base.html')
