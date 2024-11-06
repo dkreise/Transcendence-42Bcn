fec354e82502f35b1d52174efd66e04d25b82d84
Diana Kreise <dia.kreis@gmail.com>
2024-11-06 17:19:42 +0100

    created tournament table, added db to settings, added psycopg2 to requirements

--- srcs/back/game/game/settings.py
+++ srcs/back/game/game/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tournament',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,15 @@ WSGI_APPLICATION = 'game.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# CHANGE LATER TO ENV VARIABLES (?)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcendence',
+        'USER': 'admin',
+        'PASSWORD': '12345',
+        'HOST': 'db-postgres',
+        'PORT': '5432',
     }
 }
 
--- srcs/back/game/tournament/models.py
+++ srcs/back/game/tournament/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings # its for AUTH_USER_MODEL that is default user table
 
 # Create your models here.
+
+class Tournament(models.Model):
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tournaments_as_participant')
+    score = models.FloatField() # in percentage !!
