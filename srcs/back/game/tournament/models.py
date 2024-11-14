adab373ac954414829776e5d13b924b4b302ff42
Diana Kreise <dia.kreis@gmail.com>
2024-11-14 16:34:57 +0100

    added id_tournament in a table and added volume for postgres

--- srcs/back/game/tournament/models.py
+++ srcs/back/game/tournament/models.py
@@ -4,5 +4,6 @@ from django.conf import settings # its for AUTH_USER_MODEL that is default user 
 # Create your models here.
 
 class Tournament(models.Model):
+    id_tournament = models.IntegerField(default=0) # not unique 
     player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tournaments_as_participant')
     score = models.FloatField() # in percentage !!
