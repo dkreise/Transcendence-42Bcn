4e5f84f3c26b14734f84512c2d6d10690dacf8f9
dkreise <dia.kreis@gmail.com>
2024-11-15 14:39:23 +0100

    login working and created my admin

--- srcs/back/user_mgmt/login/views.py
+++ srcs/back/user_mgmt/login/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def home(request):
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect(reverse("login"))
+	return render(request, "user.html")
+		
+def login_view(request):
+	if request.method == "POST":
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
+		if user is not None: # means authentication was successful
+			login(request, user)
+			return HttpResponseRedirect(reverse("home"))
+		else:
+			return render(request, "login.html", {
+				"message": "Invalid credentials."
+			})
+	return render(request, "login.html")
+	
+def logout_view(request):
+	logout(request)
+	return render(request, "login.html", {
+		"message": "Logged out."
+	})
