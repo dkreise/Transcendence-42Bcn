2798670534399990ed9c449b37345ddca747454a
Julia Olle roset <julolle-@car7s5.42barcelona.com>
2024-11-20 16:48:30 +0100

    add new endpoints and test

--- srcs/back/user_mgmt/login/urls.py
+++ srcs/back/user_mgmt/login/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 	path("", views.home, name="home"),
 	path("login/", views.login_view, name="login"),
-	path("logout/", views.logout_view, name="logout")
+	path("logout/", views.logout_view, name="logout"),
+	path('test/', views.test_view),
+	path('new-page/', views.new_page, name='new_page'),
+	path('add-user/', views.add_user, name='add_user'),
+	path('get-users/', views.get_users, name='get_users')
 ]
--- srcs/back/user_mgmt/login/views.py
+++ srcs/back/user_mgmt/login/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
 
 def home(request):
 	if not request.user.is_authenticated:
@@ -21,9 +26,35 @@ def login_view(request):
 				"message": "Invalid credentials."
 			})
 	return render(request, "login.html")
-	
+
+def add_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        # Create the user
+        user = User.objects.create_user(username=username, password=password)
+        return render(request, "add_user.html", {"message": "User created successfully!"})
+    
+    return render(request, "add_user.html")
+
+
 def logout_view(request):
 	logout(request)
 	return render(request, "login.html", {
 		"message": "Logged out."
 	})
+
+
+def test_view(request):
+    return render(request, 'test.html')
+
+def new_page(request):
+    return render(request, 'new_page.html')
+
+
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()  # Fetch all users from the database
+    serializer = UserSerializer(users, many=True)  # Serialize the users
+    return Response(serializer.data)  
