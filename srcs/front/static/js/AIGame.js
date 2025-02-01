b769e4adb98e538adb677e51640ffe39e5e9b71c
Diana Kreise <dia.kreis@gmail.com>
2025-02-01 18:52:14 +0100

    AI algo version 1

--- srcs/front/static/js/AIGame.js
+++ srcs/front/static/js/AIGame.js
@@ -1,9 +1,4 @@
-import { makeAuthenticatedRequest } from "./login.js";
-// import { addLogoutListener } from "./logout.js";
-import { navigateTo, checkPermission } from "./main.js"
 import { Ball, Player } from "./localClasses.js";
-
-var baseUrl = "http://localhost"; // change (parse) later
 
 // Game Initialization
 let canvas = null;
@@ -14,21 +9,91 @@ let mainUser = null; // if the main user is player 1 or 2
 let ball = null;
 let gameLoopId = null;
 let maxScore = 2;
+let intervalID = null;
+let targetY = null;
+
+// if mainUser == 1, we need to focus on right wall, else -- on left wall
+// ball.xspeed > 0 -- right, < 0 -- left
+// ball.yspeed > 0 -- down, < 0 -- up
+function predictBallY() {
+    let tempX = ball.x;
+    let tempY = ball.y;
+    let tempXspeed = ball.xspeed;
+    let tempYspeed = ball.yspeed;
+
+    if (tempXspeed > 0 && mainUser == 2)
+        return null;
+    if (tempXspeed < 0 && mainUser == 1)
+        return null;
+
+    // when the ball will hit the top or bottom walls
+    let timeToTopBottom = (tempYspeed > 0) 
+        ? (canvas.height - (tempY + ball.radius)) / tempYspeed  // bottom wall
+        : -(tempY - ball.radius) / tempYspeed;                  // top wall
+
+    // when the ball will hit the left or right walls
+    let timeToLeftRight = (tempXspeed > 0) 
+        ? (canvas.width - (tempX + ball.radius)) / tempXspeed   // right wall
+        : -(tempX - ball.radius) / tempXspeed;                  // left wall
+
+    while (timeToTopBottom < timeToLeftRight) {
+    // for (let i = 0; i < 3; i++) {
+        console.log('IN THE LOOOOOOPP');
+        tempX = tempX + tempXspeed * timeToTopBottom;
+        if (tempYspeed < 0) {
+            tempY = 0;//ball.radius;
+        } else {
+            tempY = canvas.height - ball.radius;
+        }
+        tempYspeed = -tempYspeed;
+
+        timeToTopBottom = (tempYspeed > 0) 
+        ? (canvas.height - (tempY + ball.radius)) / tempYspeed  // bottom wall
+        : -(tempY - ball.radius) / tempYspeed;                  // top wall
+
+        timeToLeftRight = (tempXspeed > 0) 
+            ? (canvas.width - (tempX + ball.radius)) / tempXspeed   // right wall
+            : -(tempX - ball.radius) / tempXspeed;  
+        
+        // if (timeToTopBottom > timeToLeftRight)
+        //     break;
+    }
+
+    tempY = tempY + tempYspeed * timeToLeftRight;
+    console.log('ESTIMATED Y:');
+    console.log(tempY);
+    return tempY;
+}
 
 function doMovesAI() {
-    if (!AI.down && AI.y > 0) {
+    if (!AI || !ball)
+        return;
+
+    console.log("doing it each sec");
+    targetY = predictBallY();
+    if (targetY === null)   
+        return;
+
+    if (targetY  < AI.y + AI.height / 2) {
         AI.up = true;
-    }
-    if (AI.y <= 0) {
-        AI.up = false;
-    }
-    if (!AI.up && AI.y < AI.canvas.height - AI.height) {
+        AI.down = false;
+    } else if (targetY  > AI.y + AI.height / 2) {
         AI.down = true;
-    }
-    if (AI.y >= AI.canvas.height - AI.height) {
+        AI.up = false;
+    } else {
+        AI.up = false;
         AI.down = false;
     }
-    console.log(AI.y);
+}
+
+function checkIfAIneedStop() {
+    if (targetY) {
+        if (AI.up && AI.y + (AI.height / 2) <= targetY) {
+            AI.up = false;
+        } else if (AI.down && AI.y + (AI.height / 2) >= targetY) {
+            AI.down = false;
+        }
+    }
 }
 
 // Game loop
@@ -41,23 +106,20 @@ function gameAILoop() {
     AI.draw(ctx);
     ball.draw(ctx);
 
-    // console.log('In AI game loop!');
-
     // Draw scores
     player.drawScore(ctx);
     AI.drawScore(ctx);
 
     // Move players and ball
     player.move();
-    doMovesAI();
     AI.move();
+    checkIfAIneedStop();
     if (mainUser == 1) {
         ball.move(player, AI);
     } else {
         ball.move(AI, player);
     }
     
-
     // Endgame check
     if (player.score >= maxScore || AI.score >= maxScore) {
         const winner = player.score > AI.score ? `${player.name} Wins!` : `${AI.name} Wins!`;
@@ -66,6 +128,11 @@ function gameAILoop() {
         cancelAnimationFrame(gameLoopId);
         player.displayEndgameMessage(ctx, finalScore, winner);
         // saveScore();
+
+        if (intervalID) {
+            clearInterval(intervalID);
+            intervalID = null;
+        }
     }
 }
 
@@ -88,6 +155,10 @@ function setupControlsAI() {
 
 // Start game function
 export function startAIGame(playerName1, playerName2, mainUserNmb) {
+    if (intervalID) {
+        clearInterval(intervalID);
+        intervalID = null;
+    }
     canvas = document.getElementById("newGameCanvas");
     ctx = canvas.getContext("2d");
     console.log(mainUserNmb);
@@ -95,7 +166,7 @@ export function startAIGame(playerName1, playerName2, mainUserNmb) {
     console.log(playerName2)
 
     canvas.width = window.innerWidth * 0.65; // % of screen width
-    canvas.height = canvas.width * 0.5; // % of screen height
+    canvas.height = canvas.width * 0.57; // % of screen height
 
     mainUser = mainUserNmb;
 
@@ -109,7 +180,10 @@ export function startAIGame(playerName1, playerName2, mainUserNmb) {
         AI = new Player(canvas, 0, playerName1, 0);
     }
     ball = new Ball(canvas);
+    console.log(canvas.width);
+    console.log(canvas.height);
 
     setupControlsAI();
+    intervalID = setInterval(doMovesAI, 1000);
     gameAILoop();
 }
