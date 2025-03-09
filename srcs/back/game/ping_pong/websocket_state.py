0e1ea1f308bd7ea8db3ef8ecb31f7d906c9d6f09
Núria Ferrón Fèrriz <nuriaf207@gmail.com>
2025-03-09 12:28:08 +0100

    moved active games to websocket_state

--- srcs/back/game/ping_pong/websocket_state.py
+++ srcs/back/game/ping_pong/websocket_state.py
@@ -3,3 +3,5 @@ import asyncio
 # Store active tournaments here so it's accessible by both consumers.py and views.py
 active_tournaments = {}
 active_tournaments_lock = asyncio.Lock()
+active_games = {}
+active_games_lock = asyncio.Lock()
