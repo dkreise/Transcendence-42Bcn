d3d6a5c101bc5a2556aa9e738cdaa6fc5340d28e
Julia Olle roset <julolle-@car10s4.42barcelona.com>
2025-03-09 13:06:22 +0100

    solve routing issue after merge deploy

--- srcs/back/user_mgmt/user_mgmt/routing.py
+++ srcs/back/user_mgmt/user_mgmt/routing.py
@@ -1,7 +1,12 @@
 from django.urls import re_path
-# from user_mgmt.middleware import JwtAuthMiddleware
 from user_profile import consumers
+from django.conf import settings
+
+PROTOCOL_SOCKET = settings.PROTOCOL_SOCKET
 
 websocket_urlpatterns = [
-    re_path(r"ws/online-status/$", consumers.FriendsConsumer.as_asgi()),
+    re_path(
+        rf"{PROTOCOL_SOCKET}/online-status/$", 
+        consumers.FriendsConsumer.as_asgi()
+    ),
 ]
